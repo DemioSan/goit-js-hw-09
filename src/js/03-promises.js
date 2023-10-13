b4d622c297fccacb5b4b9ff426c15a2fa5575d5c
DemioSan <132845079+DemioSan@users.noreply.github.com>
2023-10-13 20:53:44 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+
+    try {
+      const result = await createPromise(position, currentDelay);
+      console.log(
+        `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
+      );
+    } catch (error) {
+      console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+    }
+  }
+});
